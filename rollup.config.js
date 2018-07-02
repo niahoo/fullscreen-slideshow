5846682a25166ffbe4acbe063448098eb203881e
niahoo <dev@ooha.in>
2018-07-02 10:15:37 +0700

    build with rollup

---
+++ rollup.config.js
@@ -0,0 +1,58 @@
+import svelte from 'rollup-plugin-svelte';
+import resolve from 'rollup-plugin-node-resolve';
+import commonjs from 'rollup-plugin-commonjs';
+import buble from 'rollup-plugin-buble';
+import { uglify } from 'rollup-plugin-uglify';
+import scss from 'rollup-plugin-scss'
+import { writeFileSync } from 'fs'
+
+const production = !process.env.ROLLUP_WATCH;
+const outputJs = production
+	? 'public/fsss.min.js'
+	: 'public/fsss.js'
+const outputCss = production
+	? 'public/fsss.min.css'
+	: 'public/fsss.css'
+
+
+export default {
+  input: 'src/main.js',
+  output: {
+    sourcemap: true,
+    format: 'iife',
+    name: 'app',
+    file: outputJs
+  },
+  plugins: [
+    svelte({
+      // opt in to v3 behaviour today
+      skipIntroByDefault: true,
+      nestedTransitions: true,
+
+      // enable run-time checks when not in production
+      dev: !production,
+      // we'll extract any component CSS out into
+      // a separate file — better for performance
+      css: css => {
+        css.write('public/bundle.css');
+      }
+    }),
+
+    // If you have external dependencies installed from
+    // npm, you'll most likely need these plugins. In
+    // some cases you'll need additional configuration —
+    // consult the documentation for details:
+    // https://github.com/rollup/rollup-plugin-commonjs
+    resolve(),
+    commonjs(),
+
+    scss({ output: outputCss, verbose: true }),
+
+    // If we're building for production (npm run build
+    // instead of npm run dev), transpile and minify
+    production && buble({
+      include: ['src/**', 'node_modules/svelte/shared.js']
+    }),
+    production && uglify()
+  ]
+};
